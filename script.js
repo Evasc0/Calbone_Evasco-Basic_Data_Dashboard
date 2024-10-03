1a7947a254f955691ea084099f38c94cf86bd817
Evasc0 <kishoarashi@.com>
2024-10-03 19:04:49 +0800

    Finished Introduction to Web Browsers: Basic Data Dashboard

---
+++ script.js
@@ -0,0 +1,8 @@
+const totalUsers = document.getElementById('totalUsers').innerText;
+const totalSales = document.getElementById('totalSales').innerText;
+
+const alertButton = document.getElementById('alertButton');
+
+alertButton.addEventListener('click', () => {
+    alert(`Total Users: ${totalUsers}, Total Sales: ${totalSales}`);
+});
